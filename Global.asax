4cb16e7b26edfbcbf6f8d5f5f322ff98c2eb12a3
Dan Done <daniel.done@point74.co.uk>
2023-03-06 21:33:55 +0000

    Initial Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRIssue.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,22 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR.StockTicker;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Microsoft.AspNet.SignalR.StockTicker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureSignalR(app);
+        }
+
+        public static void ConfigureSignalR(IAppBuilder app)
+        {
+            HubConfiguration hubConfiguration = new HubConfiguration();
+            hubConfiguration.EnableDetailedErrors = true;
+            app.MapSignalR(hubConfiguration);
+        }
+    }
+}
